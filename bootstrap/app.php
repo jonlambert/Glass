7689e2f783fb908b014e4d7f642d2e3b9eca978e
Jon Lambert <jonlambert94@gmail.com>
2016-03-14 16:03:31 +0000

    Added Redis support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -82,6 +82,7 @@ $app->singleton(
 // $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Dingo\Api\Provider\LumenServiceProvider::class);
 $app->register(Barryvdh\Cors\LumenServiceProvider::class);
+$app->register(Illuminate\Redis\RedisServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
