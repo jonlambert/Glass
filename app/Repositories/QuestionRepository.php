0c32bfb5e97efc5433f1bc4e8fd5acbb7acdf6b4
Jon Lambert <jonlambert94@gmail.com>
2016-03-14 16:12:44 +0000

    Added caching to question repository, as well as picking only moderated questions

---
+++ app/Repositories/QuestionRepository.php
@@ -0,0 +1,14 @@
+<?php  namespace App\Repositories;
+
+use Illuminate\Support\Facades\Cache;
+
+class QuestionRepository {
+
+	public function recent($count = 10)
+	{
+		return Cache::remember('questions.recent', 2, function() use ($count) {
+			return Question::where('state', '=', 2)->orderBy('updated_at', 'DESC')->limit($count)->get();
+		});
+	}
+
+} 
